22c304df6dde7911fd6761280c3a0ac4c99670c4
jaykee28 <jmufetu@gmail.com>
2026-05-26 16:27:15 +0200

    Fixed production routing paths

--- src/js/main.js
+++ src/js/main.js
@@ -23,7 +23,7 @@ function recipeTemplate(recipe) {
   return `
   
     <a
-      href="/src/recipe/index.html?id=${recipe.idMeal}"
+      href="/recipe/index.html?id=${recipe.idMeal}"
       class="recipe-card"
     >
 
